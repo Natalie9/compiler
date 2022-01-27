be988ed89365a71eca15037b33391113cd33f1a8
Natalie <natalietolentino9@gmail.com>
2022-01-26 21:57:50 -0300

    feat: update old JS to TS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,54 @@
-console.log('Olá TypeScript')
+import {states} from './statesTransitions'
+
+let TOKEN = {'word': {classe: '', lexema: '', tipo: ''}}
+let symbolTable = {
+    'inicio': {classe: 'inicio', lexema: 'inicio', tipo: 'inicio'},
+    'varinicio': {classe: 'varinicio', lexema: 'varinicio', tipo: 'varinicio'},
+    'varfim': {classe: 'varfim', lexema: 'varfim', tipo: 'varfim'},
+    'escreva': {classe: 'escreva', lexema: 'escreva', tipo: 'escreva'},
+    'leia': {classe: 'leia', lexema: 'leia', tipo: 'leia'},
+    'se': {classe: 'se', lexema: 'se', tipo: 'se'},
+    'entao': {classe: 'entao', lexema: 'entao', tipo: 'entao'},
+    'fimse': {classe: 'fimse', lexema: 'fimse', tipo: 'fimse'},
+    'repita': {classe: 'repita', lexema: 'repita', tipo: 'repita'},
+    'fimrepita': {classe: 'fimrepita', lexema: 'fimrepita', tipo: 'fimrepita'},
+    'fim': {classe: 'fim', lexema: 'fim', tipo: 'fim'},
+    'inteiro': {classe: 'inteiro', lexema: 'inteiro', tipo: 'inteiro'},
+    'literal': {classe: 'literal', lexema: 'literal', tipo: 'literal'},
+    'real': {classe: 'real', lexema: 'real', tipo: 'real'},
+}
+
+export function scanner(word : any) {
+    const lastState = readChain(word, states['initial'])
+    const isFinalState = checkFinalState(lastState, states['final'])
+    printAnswer(word, isFinalState)
+    return isFinalState
+}
+
+function checkFinalState(state: any, finalState: any) {
+    return state in finalState
+}
+
+function printAnswer(word: any, isFinalState: any) {
+    isFinalState ? console.log('reconhece', word) : console.log('Não reconhece', word)
+}
+
+function readChain(chain: any, initialState: any) {
+    try {
+        let state = initialState
+        chain.split('').map((letter: any) => {
+            state = readValueReturnNewState(letter, state)
+        })
+        return state
+    } catch (e) {
+        console.error(e)
+    }
+
+
+}
+
+function readValueReturnNewState(value: any, state:any) {
+    let nextState = states[state][value]
+    if (!nextState) throw 'Parameter is not a number!'; //TODO: improve throw error
+    return nextState
+}
--- src/tests.ts
+++ src/tests.ts
@@ -1,1 +1,14 @@
-console.log('teste')
+
+import {scanner} from './index'
+
+function testRecognizeNumber() {
+    console.assert(scanner('2') === true)
+    console.assert(scanner('2342342') === true, '2342342')
+    console.assert(scanner('2.3') === true)
+    console.assert(scanner('3e+3') === true)
+    console.assert(scanner('3e3') === true)
+    console.assert(scanner('3sdfs') === false)
+    console.assert(scanner('abab') === false, 'abab')
+}
+
+testRecognizeNumber()
