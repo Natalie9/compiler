4fd8a8aaa9f45d9e98379177cff8fc0e8f62050a
Isabella Beatriz <isabellabeatrizn@outlook.com>
2022-01-26 22:49:08 -0300

    feat: number test

---
+++ src/number.test.ts
@@ -0,0 +1,11 @@
+const scanner = require("./index")
+
+test('recognize number', () => {
+    expect(scanner('2')).toBe(true)
+    expect(scanner('2342342')).toBe(true)
+    expect(scanner('2.3')).toBe(true)
+    expect(scanner('3e+3')).toBe(true)
+    expect(scanner('3e3')).toBe(true)
+    expect(scanner('3sdfs')).toBe(false)
+    expect(scanner('abab')).toBe(false)
+})
