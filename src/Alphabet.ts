18ac7356605aff83c54e63d58397b7d7e63f5615
Natalie <natalietolentino9@gmail.com>
2022-01-28 00:42:07 -0300

    chore: add switch case of types

--- src/Alphabet.ts
+++ src/Alphabet.ts
@@ -1,17 +1,76 @@
 
-const PT_V = [";"]
-const QUOTES = ['"',"'"]
-const UNDERLINE = "_"
-const LESS = "<"
-const MORE = ">"
-const DOT = "."
-const PLUS = "+"
-const MINUS = "-"
-const EXPONENTIAL = ["E", "e"]
-const OPM = [PLUS, MINUS, "*", "/"]
-const EQUAL = "="
+export const PT_V = [";"]
+export const QUOTES = ['"',"'"]
+export const UNDERLINE = "_"
+export const LESS = "<"
+export const MORE = ">"
+export const DOT = "."
+export const PLUS = "+"
+export const MINUS = "-"
+export const EXPONENTIAL = ["E", "e"]
+export const OPM = [PLUS, MINUS, "*", "/"]
+export const EQUAL = "="
+export const EOF = "EOF"
+export const AB_P = "("
+export const FC_P = ")"
 export const DIGIT = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 export const LETTER = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-export const caracteres = ["{", "}", LESS, MORE, EQUAL, ...OPM, DOT, ",", ...PT_V, ":", "!", "?", "\\", "(", ")", "[", "]", UNDERLINE, ...QUOTES]
-export const ALPHABET = [...DIGIT, LETTER, caracteres, EXPONENTIAL]
+export const CARACTERES = ["{", "}", LESS, MORE, EQUAL, ...OPM, DOT, ",", ...PT_V, ":", "!", "?", "\\", AB_P, FC_P, "[", "]", UNDERLINE, ...QUOTES]
+export const ALPHABET = [...DIGIT, ...LETTER, ...CARACTERES, EXPONENTIAL]
 
+// //Constante numérica
+// const NUM = DIGIT
+// //Constante Literal
+// //@todo como separar uma constante literal de um id
+// const LIT = ['"', '"']
+// //Identificador
+// const ID = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+// //Comtentário
+// // @todo nao retornar token
+// const com = ["{", "}"]
+// //Final de arquivo
+// const eof = ["eof"]
+// //Operadores relacionais
+// const OPR = ['<', '>', '>=', '<=', '=', "<>"]
+// //atribuição
+// const RCB = ['<-']
+// //Operadores aritiméticos
+// const OPM = ["+", "-", "*", "/"]
+// //Abre parênteses
+// const AB_P = ["("]
+// //Fecha parênteses
+// const FC_P = [")"]
+// //Ponto e vírgula
+// const PT_V = [";"]
+//Erro, qualquer simbolo diferente @todo tratar
+//espaços, tab, saltos de linha..
+//@todo tratar para ignorar
+// const blank = [" ", "   ", "\n"]
+// switch (true) {
+//     case NUM.includes(value):
+//         return 'NUM'
+//     case LIT.includes(value):
+//         return 'LIT'
+//     case ID.includes(value):
+//         return 'ID'
+//     case com.includes(value):
+//         return 'com'
+//     case eof.includes(value):
+//         return 'eof'
+//     case OPR.includes(value):
+//         return 'OPR'
+//     case RCB.includes(value):
+//         return 'RCB'
+//     case OPM.includes(value):
+//         return 'OPM'
+//     case AB_P.includes(value):
+//         return 'AB_P'
+//     case FC_P.includes(value):
+//         return 'FC_P'
+//     case PT_V.includes(value):
+//         return 'PT_V'
+//     case blank.includes(value):
+//         return 'black'
+//     default:
+//         return 'ERRO'
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import {states} from './statesTransitions'
 import {isNumber} from "./utils/validations";
 import {IToken} from "./models/Token";
+import {
+    AB_P, ALPHABET,
+    DIGIT,
+    DOT,
+    EOF,
+    EQUAL,
+    EXPONENTIAL, FC_P,
+    LESS,
+    LETTER,
+    MINUS,
+    MORE,
+    OPM,
+    PLUS, PT_V,
+    QUOTES,
+    UNDERLINE
+} from "./Alphabet";
 
-let TOKEN = {'word': {classe: '', lexema: '', tipo: ''} as IToken}
 let symbolTable = {
     'inicio': {classe: 'inicio', lexema: 'inicio', tipo: 'inicio'},
     'varinicio': {classe: 'varinicio', lexema: 'varinicio', tipo: 'varinicio'},
@@ -37,7 +52,7 @@ function checkFinalState(state: any, finalState: any) {
 
 function printAnswer(word: string, lastState: string) {
     const isFinalState = checkFinalState(lastState, states['final'])
-    if(isFinalState) {
+    if (isFinalState) {
         console.log(states['final'][lastState])
         return true
     } else {
@@ -48,7 +63,7 @@ function printAnswer(word: string, lastState: string) {
 }
 
 
-function readText(text: string, initialState: any) {
+function readText(text: string, initialState: string) {
     const textAsArray = text.split('')
     let lexema = ''
     let state = initialState
@@ -57,25 +72,56 @@ function readText(text: string, initialState: any) {
         if (caractere == ' ') break
         lexema += caractere
         state = readValueReturnNewState(caractere, state)
-        if(state== 'qErro') break
+        if (state == 'qErro') break
     }
     return {lastState: state, lexema}
-
 
 }
 
 function turnValueInSomeGeneralType(value: any) {
-    //todo: colocar mais tipos genéricos, exemplo letra
+    switch (true) {
+        case DIGIT.includes(value):
+            return 'DIGIT'
+        case LETTER.includes(value):
+            return 'LETTER'
+        case QUOTES.includes(value):
+            return 'QUOTES'
+        case UNDERLINE.includes(value):
+            return 'UNDERLINE'
+        case LESS.includes(value):
+            return 'LESS'
+        case MORE.includes(value):
+            return 'MORE'
+        case DOT.includes(value):
+            return 'DOT'
+        case PLUS.includes(value):
+            return 'PLUS'
+        case MINUS.includes(value):
+            return 'MINUS'
+        case EXPONENTIAL.includes(value):
+            return 'EXPONENTIAL'
+        case OPM.includes(value):
+            return 'OPM'
+        case EOF.includes(value):
+            return 'EOF'
+        case EQUAL.includes(value):
+            return 'EQUAL'
+        case AB_P.includes(value):
+            return 'AB_P'
+        case FC_P.includes(value):
+            return 'FC_P'
+        case PT_V.includes(value):
+            return 'PT_V'
+        case ALPHABET.includes(value):
+            return 'ALPHABET'
+        default:
+            return 'ERRO'
 
-    if (isNumber(value))
-        return 'number'
-    else if (value == ' ')
-        return 'blank'
-    else return value
+    }
 
 }
 
-function readValueReturnNewState(value: any, state: any) {
+function readValueReturnNewState(value: string, state: string) {
     const input = turnValueInSomeGeneralType(value)
     console.log(input)
     let nextState = states[state][input]
