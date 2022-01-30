438b7336c3cde1b92afd11d5cff7047e53e9cf6c
Isabella Beatriz <isabellabeatrizn@outlook.com>
2022-01-30 10:06:30 -0300

    chore: Add all number token test scenarios

--- src/__tests__/number.test.ts
+++ src/__tests__/number.test.ts
@@ -1,21 +1,32 @@
 import {scanner} from "../index";
+import { IToken } from "../models/Token";
 
 describe('Test scanner read some source code', () => {
-    it('return first token as number', () => {
-        expect(scanner('2')).toStrictEqual({"position": 1, "token": {"classe": "NUM", "lexema": "2", "tipo": "inteiro"}})
-       
+    it('Return first token as integer', () => {
+        const text = '123sfd'
+        const expectedTokenNumber: IToken = {lexema: '123', tipo: 'inteiro', classe: 'NUM'}
+        let {token} = scanner(text)
+        expect(token).toEqual(expectedTokenNumber)
     }),
-    it('recognize decimal number', () => {
-        let response
-        response = scanner('3e+3')
+    it('Recognize exponential number with signal', () => {
+        const text = '3e+3'
+        const expectedTokenNumber: IToken = {lexema: text, tipo: 'real', classe: 'NUM'}
+        let {token} = scanner(text)
+        expect(token).toEqual(expectedTokenNumber)
+    }),
+    it('Recognize exponential number', () => {
+        const text = '3e3'
+        const expectedTokenNumber: IToken = {lexema: text, tipo: 'real', classe: 'NUM'}
+        let {token} = scanner(text)
+        expect(token).toEqual(expectedTokenNumber)
+    }),
+    it('Recognize real number', () => {
+        const text = '3.3'
+        const expectedTokenNumber: IToken = {lexema: text, tipo: 'real', classe: 'NUM'}
+        let {token} = scanner(text)
+        expect(token).toEqual(expectedTokenNumber)
     })
 })
 
-//it('', () => {
-  //  expect(scanner('2342342')).toStrictEqual({"position": 1, "token": {"classe": "NUM", "lexema": "2", "tipo": "inteiro"}})
-//}),           
-// expect(scanner('2.3')).toBe(true)
-        // expect(scanner('3e+3')).toBe(true)
-        // expect(scanner('3e3')).toBe(true)
-        // expect(scanner('3sdfs')).toBe(false)
-        // expect(scanner('abab')).toBe(false))e+e+
+
+   
