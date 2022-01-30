2b6a54e03f28f0c4ee8e55406eda61ed6d09fcb3
Isabella Beatriz <isabellabeatrizn@outlook.com>
2022-01-30 09:54:03 -0300

    chore: improve number test

--- src/__tests__/number.test.ts
+++ src/__tests__/number.test.ts
@@ -1,11 +1,21 @@
 import {scanner} from "../index";
 
-test('recognize number', () => {
-    expect(scanner('2')).toBe(true)
-    // expect(scanner('2342342')).toBe(true)
-    // expect(scanner('2.3')).toBe(true)
-    // expect(scanner('3e+3')).toBe(true)
-    // expect(scanner('3e3')).toBe(true)
-    // expect(scanner('3sdfs')).toBe(false)
-    // expect(scanner('abab')).toBe(false)
+describe('Test scanner read some source code', () => {
+    it('return first token as number', () => {
+        expect(scanner('2')).toStrictEqual({"position": 1, "token": {"classe": "NUM", "lexema": "2", "tipo": "inteiro"}})
+       
+    }),
+    it('recognize decimal number', () => {
+        let response
+        response = scanner('3e+3')
+    })
 })
+
+//it('', () => {
+  //  expect(scanner('2342342')).toStrictEqual({"position": 1, "token": {"classe": "NUM", "lexema": "2", "tipo": "inteiro"}})
+//}),           
+// expect(scanner('2.3')).toBe(true)
+        // expect(scanner('3e+3')).toBe(true)
+        // expect(scanner('3e3')).toBe(true)
+        // expect(scanner('3sdfs')).toBe(false)
+        // expect(scanner('abab')).toBe(false))e+e+
